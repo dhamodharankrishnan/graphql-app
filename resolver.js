c1f15de07d6b32e2040483a2807b4bdfe6f36e6d
sdhamodharan-allioc <sdhamodharan@allioc.com>
2022-04-01 14:35:20 +0530

    Up to date tutorials

--- resolver.js
+++ resolver.js
@@ -3,22 +3,73 @@ const db = require('./db')
 const Query = {
     test: () => 'Test Success, GraphQL server is up & running !!',
     greeting: () => 'Hello GraphQL  From TutorialsPoint !!',
+    greetingWithAuth:(root,args,context,info) => {
+
+        //check if the context.user is null
+        if (!context.user) {
+           throw new Error('Unauthorized');
+        }
+        return "Hello from TutorialsPoint, welcome back : " + context.user.firstName;
+     },
     studentDetails: () => { 
         console.log('resolver studentDetails: ', db.students);
         return db.students.list() 
     },
+    collegeDetails: () => {
+        console.log('resolver collegeDetails:', db.colleges);
+        return db.colleges.list();
+    },
      //resolver function for studentbyId
      studentById:(root,args,context,info) => {
         return db.students.get(args.id);
+     },
+     //Say Hello with params
+     sayHello:(root,args,context,info) => `Hi ${args.name}! GraphQL server says Hello to you!!`,
+     setFavouriteColor: (root, args) => {
+        return  "Your Fav Color is :" + args.color;
      }
+
  }
 
  const Mutation = {
-    addStudent: (firstName, lastName) => {
-        console.log('firstName :',firstName);
-        console.log('lastName :',lastName);
-    }
+    addStudent: (root,args,context,info) => {
+        console.log('addStudent Mutation resolver');
+        console.log('firstName :', args.firstName);
+        console.log('lastName :', args.lastName);
+        return db.students.create({collegeId: args.collegeId,
+        firstName: args.firstName, lastName: args.lastName})
+    },
+    //create Student
+    createStudent: (root, args, context, info) => {
+        const studId = db.students.create({
+            collegeId: args.collegeId, 
+            firstName: args.firstName,
+            lastName: args.lastName
+        })
+        console.log('createStudent : studId', studId);
+        return db.students.get(studId);
+    },
+    //Sign up a new user.
+    signUp:(root,args,context,info) => {
 
+        const {email,firstName,password} = args.input;
+  
+        const emailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        const isValidEmail =  emailExpression.test(String(email).toLowerCase())
+        if(!isValidEmail)
+        throw new Error("email not in proper format")
+  
+        if(firstName.length > 15)
+        throw new Error("firstName should be less than 15 characters")
+  
+        if(password.length < 8 )
+        throw new Error("password should be minimum 8 characters")
+        
+        return "success";
+     }
+
+    
  }
 
  //for each single student object returned,resolver is invoked
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const db = require('./db');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
+
+
+//private key
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const port = process.env.PORT || 9000;
 const app = express();
@@ -15,9 +22,40 @@ const schema = makeExecutableSchema({typeDefs, resolvers})
 
 app.use(cors(), bodyParser.json());
 
-const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
-app.use('/graphql',graphqlExpress({schema}))
+// app.use('/graphql',graphqlExpress({schema}))
+// app.use('/graphql', graphqlExpress((req) => ({
+//    schema,
+//    context: {user: req.user } // , context:db.students.get(req.user.sub)
+// })));
+app.use('/graphql', graphqlExpress((req) => ({
+   schema,
+   context: {user: db.students.get(req.user) }
+})));
+
+
 app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
+
+app.post('/login', (req, res) => {
+   const {email, password} = req.body;
+   
+   //check database
+   const user = db.students.list().find((user) =>  user.email === email);
+   if (!(user && user.password === password)) {
+      res.sendStatus(401);
+      return;
+   }
+   
+   //generate a token based on private key, token doesn't have an expiry
+   const token = jwt.sign({sub: user.id}, jwtSecret);
+   res.send({token});
+});
+
+//decodes the JWT and stores in request object
+app.use(expressJwt({
+   secret: jwtSecret,
+   credentialsRequired: false,
+   algorithms: ['RS256']
+}));
 
 app.listen(
    port, () => console.info(
